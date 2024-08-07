9b0fbdd53b34881d520835b8f90281d224d2d3f7
Josh Engels <jengels@mit.edu>
2024-08-06 22:29:06 -0400

    Saving plots

--- sae_multid_feature_discovery/other_circle_points.py
+++ sae_multid_feature_discovery/other_circle_points.py
@@ -20,130 +20,158 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
     cache_dir="/media/MODELS",
 )
-
 # %%
 
-hidden_states = []
-texts = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-    "the weekend",
-    "very late on Monday",
-    "very late on Tuesday",
-    "very late on Wednesday",
-    "very late on Thursday",
-    "very late on Friday",
-    "very late on Saturday",
-    "very late on Sunday",
-    "very early on Monday",
-    "very early on Tuesday",
-    "very early on Wednesday",
-    "very early on Thursday",
-    "very early on Friday",
-    "very early on Saturday",
-    "very early on Sunday",
+standard_texts = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "the weekend"
 ]
 
-for day in texts:
-    prompt = f"{day}"
+experiments = []
 
-    inputs = tokenizer(prompt, return_tensors="pt")
+extra_text = ""
+flip_axis = True
 
-    outputs = model(**inputs, output_hidden_states=True)
+experiment_1 = []
+for day in standard_texts:
+    experiment_1.append(f"{extra_text}{day}")
+for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]:
+    for time in ["morning", "evening"]:
+        experiment_1.append(f"{extra_text}{time} on {day}")
+experiments.append(experiment_1)
 
-    hidden_states.append(
-        [outputs.hidden_states[layer][0][-1].cpu().numpy() for layer in range(len(outputs.hidden_states))]
-    )
+experiment_2 = []
+for day in standard_texts:
+    experiment_2.append(f"{extra_text}{day}")
+for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]:
+    for time in ["very early", "very late"]:
+        experiment_2.append(f"{extra_text}{time} on {day}")
+experiments.append(experiment_2)
+
+for texts, name in zip(experiments, ["experiment_1", "experiment_2"]):
+    hidden_states = []
+    for day in texts:
+        prompt = f"{day}"
+
+        inputs = tokenizer(prompt, return_tensors="pt")
+
+        outputs = model(**inputs, output_hidden_states=True)
+
+        hidden_states.append(
+            [outputs.hidden_states[layer][0][-1].cpu().numpy() for layer in range(len(outputs.hidden_states))]
+        )
 
 
-# %%
-
-hidden_states = np.array(hidden_states)
-
-skip_up_to = 0
-
-# for layer in range(81):
-# for layer in range(62, 63):
-# for layer in range(8, 9):
-for layer in range(30, 31):
-
-    fig, ax1 = plt.subplots()
-
-    layer_hidden_states = hidden_states[:, layer]
-
-    pca = PCA(n_components=5)
-    pca.fit(layer_hidden_states[:7])
-
-    layer_hidden_states_pca = pca.transform(layer_hidden_states)
-
-    percent_explained = pca.explained_variance_ratio_[:2].sum()
-
-    dims_to_plot = [0, 1]
-
-    ax1.scatter(layer_hidden_states_pca[skip_up_to:, dims_to_plot[0]], layer_hidden_states_pca[skip_up_to:, dims_to_plot[1]])
-    text_labels = []
-    for i, txt in enumerate(texts):
-        if i >= skip_up_to:
-            text = ax1.annotate(
-                txt, (layer_hidden_states_pca[i, dims_to_plot[0]], layer_hidden_states_pca[i, dims_to_plot[1]])
-            )
-            text_labels.append(text)
-
-    def change_position(i, delta_x, delta_y):
-        text_labels[i].set_position((text_labels[i].get_position()[0] + delta_x, text_labels[i].get_position()[1] + delta_y))
+    hidden_states = np.array(hidden_states)
 
     for i in range(len(texts)):
-        change_position(i, 0.25, 0)
+        texts[i] = texts[i][len(extra_text):]
+
+    special = range(7)
+    # special = []
+    skip_up_to = 0
+
+    for layer in range(30, 31):
+
+        fig, ax1 = plt.subplots()
+
+        layer_hidden_states = hidden_states[:, layer]
+
+        pca = PCA(n_components=5)
+        pca.fit(layer_hidden_states[:7])
+
+        print(pca.explained_variance_ratio_)
+
+        layer_hidden_states_pca = pca.transform(layer_hidden_states)[:, :2]
+
+        ax1.axis('equal')
+
+        percent_explained = pca.explained_variance_ratio_[:2].sum()
+
+        dims_to_plot = [0, 1]
+
+        ax1.scatter(layer_hidden_states_pca[skip_up_to:, dims_to_plot[0]], layer_hidden_states_pca[skip_up_to:, dims_to_plot[1]])
+        
+        ax1.scatter(layer_hidden_states_pca[special, dims_to_plot[0]], layer_hidden_states_pca[special, dims_to_plot[1]], color='red', s=40)
+
+        text_labels = []
+        for i, txt in enumerate(texts):
+            if i >= skip_up_to:
+                if i in special:
+                    text = ax1.annotate(
+                        txt, (layer_hidden_states_pca[i, dims_to_plot[0]], layer_hidden_states_pca[i, dims_to_plot[1]]),
+                        color='red',
+                        fontsize=12
+                    )
+                else:
+                    text = ax1.annotate(
+                        txt, (layer_hidden_states_pca[i, dims_to_plot[0]], layer_hidden_states_pca[i, dims_to_plot[1]]),
+                        fontsize=9
+                    )
+                text_labels.append(text)
+
+            
+
+        def change_position(i, delta_x, delta_y):
+            text_labels[i].set_position((text_labels[i].get_position()[0] + delta_x, text_labels[i].get_position()[1] + delta_y))
+
+        for i in range(len(texts)):
+            change_position(i, 0.25, 0)
 
 
-    # Thursday
-    change_position(3, -1.7, 0.5)
+        # # Thursday
+        # change_position(3, -1.7, 0.5)
 
-    # very early on Sunday
-    change_position(-1, -0.2, -.7)
+        if "very early on Friday" in texts:
+            change_position(texts.index("very early on Friday"), -0.3, -0.2)
+        if "very late on Friday" in texts:
+            change_position(texts.index("very late on Friday"), 0, 0.2)
 
-    # Wednesday
-    change_position(2, 0, -0.5)
 
-    change_position(8, 0, -0.6)
+        # # very early on Sunday
+        # change_position(-1, -0.2, -.7)
 
-    # very early on Tuesday
-    change_position(16, -7, -0.5)
+        # # Wednesday
+        # change_position(2, 0, -0.5)
 
-    change_position(15, 0, 0.3)
+        # change_position(8, 0, -0.6)
 
-    # Very late on wednesday
-    change_position(10, -1.9, -0.8)
+        # # very early on Tuesday
+        # change_position(16, -7, -0.5)
 
-    # Very late on friday
-    change_position(12, 0, -0.5)
+        # change_position(15, 0, 0.3)
 
-    # Very early on Friday
-    change_position(19, -6, -0.7)
+        # # Very on wednesday
+        # change_position(10, -1.9, -0.8)
 
-    # Remove outer axis
-    ax1.spines['top'].set_visible(False)
-    ax1.spines['right'].set_visible(False)
-    ax1.spines['left'].set_visible(False)
-    ax1.spines['bottom'].set_visible(False)
-    ax1.set_xticks([])
-    ax1.set_yticks([])
+        # # Very on friday
+        # change_position(12, 0, -0.5)
 
-    # Add center axis
-    ax1.axhline(0, color='black', lw=0.5)
-    ax1.axvline(0, color='black', lw=0.5)
+        # # Very early on Friday
+        # change_position(19, -6, -0.7)
 
-    # Make plot square
-    ax1.set_aspect('equal', adjustable='box')
+        # Remove outer axis
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['left'].set_visible(False)
+        ax1.spines['bottom'].set_visible(False)
+        ax1.set_xticks([])
+        ax1.set_yticks([])
 
-    plt.title(f"Mistral Layer {layer} Projected into Weekday Plane")
+        # Add center axis
+        ax1.axhline(0, color='black', lw=0.5)
+        ax1.axvline(0, color='black', lw=0.5)
 
-    plt.show()
+        # Make plot square
+        # ax1.set_aspect('equal', adjustable='box')
+
+
+        plt.title(f"Mistral Layer {layer} Projected into Weekday Plane")
+
+        if flip_axis:
+            plt.gca().invert_yaxis()
+
+        # Save plot
+        plt.savefig(f"mistral_weekdays_{name}.pdf", bbox_inches='tight')
 
 # %%
 
@@ -178,18 +206,18 @@ texts = [
     # "mid October",
     # "mid November",
     # "mid December",
-    # "late January",
-    # "late February",
-    # "late March",
-    # "late April",
-    # "late May",
-    # "late June",
-    # "late July",
-    # "late August",
-    # "late September",
-    # "late October",
-    # "late November",
-    # "late December",
+    # "January",
+    # "February",
+    # "March",
+    # "April",
+    # "May",
+    # "June",
+    # "July",
+    # "August",
+    # "September",
+    # "October",
+    # "November",
+    # "December",
     # "early January",
     # "early February",
     # "early March",
