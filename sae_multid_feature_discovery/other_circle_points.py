2a0580abbbcb169ae0339a2a7084635afb8c8705
Joshua Engels <jengels@mit.edu>
2024-09-30 11:57:45 -0400

    Updating plot

--- sae_multid_feature_discovery/other_circle_points.py
+++ sae_multid_feature_discovery/other_circle_points.py
@@ -18,7 +18,7 @@ tokenizer = AutoTokenizer.from_pretrained(
 model = AutoModelForCausalLM.from_pretrained(
     "mistralai/Mistral-7B-v0.1",
     device_map="auto",
-    cache_dir="/media/MODELS",
+    # cache_dir="/media/MODELS",
 )
 # %%
 
@@ -48,18 +48,17 @@ for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", 
 experiments.append(experiment_2)
 
 for texts, name in zip(experiments, ["experiment_1", "experiment_2"]):
+
+    fig, ax = plt.subplots(1, 1, figsize=(5.5*4/10, 1.8))  # Create a figure with two subplots side by side
+
     hidden_states = []
     for day in texts:
         prompt = f"{day}"
-
         inputs = tokenizer(prompt, return_tensors="pt")
-
         outputs = model(**inputs, output_hidden_states=True)
-
         hidden_states.append(
             [outputs.hidden_states[layer][0][-1].cpu().numpy() for layer in range(len(outputs.hidden_states))]
         )
-
 
     hidden_states = np.array(hidden_states)
 
@@ -67,111 +66,77 @@ for texts, name in zip(experiments, ["experiment_1", "experiment_2"]):
         texts[i] = texts[i][len(extra_text):]
 
     special = range(7)
-    # special = []
     skip_up_to = 0
 
-    for layer in range(30, 31):
+    layer = 30  # Assuming we're only interested in layer 30 as in the original code
 
-        fig, ax1 = plt.subplots()
+    layer_hidden_states = hidden_states[:, layer]
 
-        layer_hidden_states = hidden_states[:, layer]
+    pca = PCA(n_components=5)
+    pca.fit(layer_hidden_states[:7])
 
-        pca = PCA(n_components=5)
-        pca.fit(layer_hidden_states[:7])
+    print(f"{name} explained variance ratio:", pca.explained_variance_ratio_)
 
-        print(pca.explained_variance_ratio_)
+    layer_hidden_states_pca = pca.transform(layer_hidden_states)[:, :2]
 
-        layer_hidden_states_pca = pca.transform(layer_hidden_states)[:, :2]
+    ax.axis('equal')
 
-        ax1.axis('equal')
+    percent_explained = pca.explained_variance_ratio_[:2].sum()
 
-        percent_explained = pca.explained_variance_ratio_[:2].sum()
+    dims_to_plot = [0, 1]
 
-        dims_to_plot = [0, 1]
+    ax.scatter(layer_hidden_states_pca[skip_up_to:, dims_to_plot[0]], layer_hidden_states_pca[skip_up_to:, dims_to_plot[1]], s=1)
+    ax.scatter(layer_hidden_states_pca[special, dims_to_plot[0]], layer_hidden_states_pca[special, dims_to_plot[1]], color='red', s=3)
 
-        ax1.scatter(layer_hidden_states_pca[skip_up_to:, dims_to_plot[0]], layer_hidden_states_pca[skip_up_to:, dims_to_plot[1]])
-        
-        ax1.scatter(layer_hidden_states_pca[special, dims_to_plot[0]], layer_hidden_states_pca[special, dims_to_plot[1]], color='red', s=40)
+    text_labels = []
+    for i, txt in enumerate(texts):
+        if i >= skip_up_to:
+            if i in special:
+                text = ax.annotate(
+                    txt, (layer_hidden_states_pca[i, dims_to_plot[0]], layer_hidden_states_pca[i, dims_to_plot[1]]),
+                    color='red',
+                    fontsize=7
+                )
+            else:
+                text = ax.annotate(
+                    txt, (layer_hidden_states_pca[i, dims_to_plot[0]], layer_hidden_states_pca[i, dims_to_plot[1]]),
+                    fontsize=5
+                )
+            text_labels.append(text)
 
-        text_labels = []
-        for i, txt in enumerate(texts):
-            if i >= skip_up_to:
-                if i in special:
-                    text = ax1.annotate(
-                        txt, (layer_hidden_states_pca[i, dims_to_plot[0]], layer_hidden_states_pca[i, dims_to_plot[1]]),
-                        color='red',
-                        fontsize=12
-                    )
-                else:
-                    text = ax1.annotate(
-                        txt, (layer_hidden_states_pca[i, dims_to_plot[0]], layer_hidden_states_pca[i, dims_to_plot[1]]),
-                        fontsize=9
-                    )
-                text_labels.append(text)
+    def change_position(i, delta_x, delta_y):
+        text_labels[i].set_position((text_labels[i].get_position()[0] + delta_x, text_labels[i].get_position()[1] + delta_y))
+        # pass
 
-            
+    for i in range(len(texts)):
+        change_position(i, 0.25, 0)
 
-        def change_position(i, delta_x, delta_y):
-            text_labels[i].set_position((text_labels[i].get_position()[0] + delta_x, text_labels[i].get_position()[1] + delta_y))
+    if "very early on Friday" in texts:
+        change_position(texts.index("very early on Friday"), -0.3, -0.2)
+    if "very late on Friday" in texts:
+        change_position(texts.index("very late on Friday"), 0, 0.2)
 
-        for i in range(len(texts)):
-            change_position(i, 0.25, 0)
+    # Remove outer axis
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
 
+    # Add center axis
+    ax.axhline(0, color='black', lw=0.5)
+    ax.axvline(0, color='black', lw=0.5)
 
-        # # Thursday
-        # change_position(3, -1.7, 0.5)
+    # ax.set_title(f"Mistral Layer {layer} - {name}")
 
-        if "very early on Friday" in texts:
-            change_position(texts.index("very early on Friday"), -0.3, -0.2)
-        if "very late on Friday" in texts:
-            change_position(texts.index("very late on Friday"), 0, 0.2)
+    if flip_axis:
+        ax.invert_yaxis()
 
-
-        # # very early on Sunday
-        # change_position(-1, -0.2, -.7)
-
-        # # Wednesday
-        # change_position(2, 0, -0.5)
-
-        # change_position(8, 0, -0.6)
-
-        # # very early on Tuesday
-        # change_position(16, -7, -0.5)
-
-        # change_position(15, 0, 0.3)
-
-        # # Very on wednesday
-        # change_position(10, -1.9, -0.8)
-
-        # # Very on friday
-        # change_position(12, 0, -0.5)
-
-        # # Very early on Friday
-        # change_position(19, -6, -0.7)
-
-        # Remove outer axis
-        ax1.spines['top'].set_visible(False)
-        ax1.spines['right'].set_visible(False)
-        ax1.spines['left'].set_visible(False)
-        ax1.spines['bottom'].set_visible(False)
-        ax1.set_xticks([])
-        ax1.set_yticks([])
-
-        # Add center axis
-        ax1.axhline(0, color='black', lw=0.5)
-        ax1.axvline(0, color='black', lw=0.5)
-
-        # Make plot square
-        # ax1.set_aspect('equal', adjustable='box')
-
-
-        plt.title(f"Mistral Layer {layer} Projected into Weekday Plane")
-
-        if flip_axis:
-            plt.gca().invert_yaxis()
-
-        # Save plot
-        plt.savefig(f"mistral_weekdays_{name}.pdf", bbox_inches='tight')
+    plt.tight_layout()
+    plt.savefig(f"mistral_weekdays_{name}.pdf", bbox_inches='tight')
+    plt.show()
+    plt.close()
 
 # %%
 
